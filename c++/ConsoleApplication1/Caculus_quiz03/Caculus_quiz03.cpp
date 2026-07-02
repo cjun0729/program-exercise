e22c4deee0fb42d1e410231057efbf9143642a0f
cjun0729 <chen0909929197@gmail.com>
2026-07-02 22:01:22 +0800

    新增創建函數與調用函數的練習

--- c++/ConsoleApplication1/Caculus_quiz03/Caculus_quiz03.cpp
+++ c++/ConsoleApplication1/Caculus_quiz03/Caculus_quiz03.cpp
@@ -1,5 +1,32 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>;
+//第一題，轉置矩陣
+//第二題，矩陣逆置，要求使用自訂函數，void=泛性指標;不return
+//第二題函數要求，Print打印，Reverse逆置
+void Print(int arr[], int len) {
+	for (int i=0; i < len;i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+void Reverse(int arr[], int len) {
+	int temp;//事前處理
+	int i = 0;
+	for (int i = 0; i < len/2;i++) {//如果超過一半會重複對調
+		int j = len - 1 - i;
+		temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	for (int i = 0; i < len;i++) {
+		printf("%d ", arr[i]);
+	}
+	
+}
+//-------------------------------------------------------
+
 int main()
 {//3*3矩陣轉置
 /*{1,2,3},
@@ -10,7 +37,7 @@ int main()
 * {2,5,8},
 * {3,6,9}
 */
-	int arr[3][3] = {
+	int arr0[3][3] = {
 		{1,2,3},
 		{4,5,6},
 		{7,8,9}
@@ -22,7 +49,7 @@ int main()
 	for (int i = 0;i < 3;i++) {//處理row
 		for (int j = 0;j < 3;j++) {//處理colum
 			
-			arr1[j][i] = arr[i][j];
+			arr1[j][i] = arr0[i][j];
 			
 		}
 	}
@@ -34,6 +61,11 @@ int main()
 		printf("\n");
 	}
 
+	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	//呼叫函數
+	Print(arr, len);
+	Reverse(arr, len);
 
 	return 0;
 }
